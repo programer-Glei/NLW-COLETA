2ae8b2c81e81547b20410fd585f19bdbca7076f2
Gleibson Pereira <gleibson.santos05@gmail.com>
2021-02-21 19:25:51 -0300

    oitavo comit

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -1,2 +1,34 @@
 
-document.querySelector("select[name=uf]").addEventListener("change", () => {})
+function acharEstados(){
+    let ufSeelecionado = document.querySelector("select[name=uf]")
+
+    fetch("https://servicodados.ibge.gov.br/api/v1/localidades/estados").then(res => res.json()).then(estados => {
+
+        for(estado of estados){
+            ufSeelecionado.innerHTML += `<option value="${estado.id}">${estado.nome}</option>`
+        }
+    })
+}
+
+acharEstados()
+
+function pegarCidades(event){
+    let cidades = document.querySelector("select[name=cidade")
+
+    let uf = event.target.value
+    console.log(uf)
+
+    let url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${uf}/municipios`
+
+    fetch(url).then(res => res.json()).then(cities => {
+        cidades.innerHTML = ""
+        cidades.innerHTML = "<option>Selecione a cidade</option>"
+        for(cidade of cities){
+            cidades.innerHTML += `<option value="${cidade.id}">${cidade.nome}</option>`
+        }
+        cidades.disabled = false
+    })
+}
+
+
+document.querySelector("select[name=uf]").addEventListener("change", pegarCidades)
